f1477f5ad913e4c43d48147865c806f1eb51fc4d
Artur Pedro <arturpedromartins@gmail.com>
2024-02-19 13:11:05 -0300

    feat: View command implemented

--- src/com/acelerazg/cli_app/Application.groovy
+++ src/com/acelerazg/cli_app/Application.groovy
@@ -1,10 +1,14 @@
 package com.acelerazg.cli_app
 
 import com.acelerazg.file_manager.UsersFileManager
+// import com.acelerazg.users.MatchChecker
+// import com.acelerazg.users.UserHandler
 
 UsersFileManager fileManager = new UsersFileManager()
+// UserHandler userHandler = new UserHandler()
 
 def all_users = fileManager.readLog()
+
 PrinterCLI.printMenu()
 while (true) {
     PrinterCLI.askCommand()
@@ -12,12 +16,17 @@ while (true) {
     if (command == "close") break
     switch (command) {
         case "view": // must show all users, separated by type
+            PrinterCLI.viewUsers(all_users)
             break
-        case "add": // must add a new user
+        case "new": // must add a new user
+        //    userHandler.newUser(all_users)
             break
         case "like": // choose an user to like another
+        //    userHandler.liking()
             break
         case "check": // check an user's likes and matches
+        //    MatchChecker matchChecker = new MatchChecker()
+        //    matchChecker.checkMatch()
             break
         default: println "Invalid command"
     }
