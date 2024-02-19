028666124479b2902f54db61d7811579261705b1
Artur Pedro <arturpedromartins@gmail.com>
2024-02-19 13:10:06 -0300

    feat: object construction added to readLog()

--- src/com/acelerazg/file_manager/UsersFileManager.groovy
+++ src/com/acelerazg/file_manager/UsersFileManager.groovy
@@ -1,18 +1,29 @@
 package com.acelerazg.file_manager
 
+import com.acelerazg.users.Candidate
+import com.acelerazg.users.Company
 import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
 
 class UsersFileManager {
-    def path = "../../../log/users.json"
+    def path = "../../../../log/users.json"
     def log = new File(path)
 
     def readLog() {
         if (log.exists()) {
             def json = new JsonSlurper()
-            return json.parseText(log.text)
+            Map users =json.parseText(log.text)
+            def users_obj = [company:[],candidate:[]]
+            users['candidate'].forEach { c ->
+                users_obj["candidate"] << new Candidate(c)
+            }
+            users['company'].forEach { c ->
+                users_obj["company"] << new Company(c)
+            }
+            return users_obj
         }
-        return [company:[:],candidates:[:]] // empty users
+        log.createNewFile()
+        [company:[],candidates:[]] // empty users
     }
     def writeLog(Map users) {
         log.createNewFile()
