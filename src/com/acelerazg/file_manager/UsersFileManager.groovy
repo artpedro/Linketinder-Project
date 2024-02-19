f7fc6eb4c582327d83b041382edb647cfb8ac462
Artur Pedro <arturpedromartins@gmail.com>
2024-02-19 01:00:07 -0300

    feat: Read and Write JSON into log file

--- src/com/acelerazg/file_manager/UsersFileManager.groovy
+++ src/com/acelerazg/file_manager/UsersFileManager.groovy
@@ -1,11 +1,22 @@
 package com.acelerazg.file_manager
 
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+
 class UsersFileManager {
-    def path = "./log/users.json"
+    def path = "../../../log/users.json"
     def log = new File(path)
 
     def readLog() {
+        if (log.exists()) {
+            def json = new JsonSlurper()
+            return json.parseText(log.text)
+        }
+        return [company:[:],candidates:[:]] // empty users
     }
-    def writeLog() {
+    def writeLog(Map users) {
+        log.createNewFile()
+        def jsonString = new JsonBuilder(users).toPrettyString()
+        log.withWriter {writer -> writer << jsonString}
     }
 }
