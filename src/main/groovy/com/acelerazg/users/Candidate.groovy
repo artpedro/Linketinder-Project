9b95c5efc8498bd81a31e4227f209f8307863866
Artur Pedro <arturpedromartins@gmail.com>
2024-02-23 23:05:06 -0300

    feat: added equals and hashCode methods

--- src/main/groovy/com/acelerazg/users/Candidate.groovy
+++ src/main/groovy/com/acelerazg/users/Candidate.groovy
@@ -25,6 +25,35 @@ class Candidate implements User{
         this.likes = map['likes']
     }
 
+    boolean equals(o) {
+        if (this.is(o)) return true
+        if (!(o instanceof Candidate)) return false
+
+        Candidate candidate = (Candidate) o
+
+        if (age != candidate.age) return false
+        if (com_acelerazg_users_User__CEP != candidate.com_acelerazg_users_User__CEP) return false
+        if (com_acelerazg_users_User__CPF_CNPJ != candidate.com_acelerazg_users_User__CPF_CNPJ) return false
+        if (com_acelerazg_users_User__country != candidate.com_acelerazg_users_User__country) return false
+        if (com_acelerazg_users_User__desc != candidate.com_acelerazg_users_User__desc) return false
+        if (com_acelerazg_users_User__email != candidate.com_acelerazg_users_User__email) return false
+        if (com_acelerazg_users_User__name != candidate.com_acelerazg_users_User__name) return false
+
+        return true
+    }
+
+    int hashCode() {
+        int result
+        result = age
+        result = 31 * result + (com_acelerazg_users_User__name != null ? com_acelerazg_users_User__name.hashCode() : 0)
+        result = 31 * result + (com_acelerazg_users_User__desc != null ? com_acelerazg_users_User__desc.hashCode() : 0)
+        result = 31 * result + (com_acelerazg_users_User__email != null ? com_acelerazg_users_User__email.hashCode() : 0)
+        result = 31 * result + (com_acelerazg_users_User__country != null ? com_acelerazg_users_User__country.hashCode() : 0)
+        result = 31 * result + (com_acelerazg_users_User__CEP != null ? com_acelerazg_users_User__CEP.hashCode() : 0)
+        result = 31 * result + (com_acelerazg_users_User__CPF_CNPJ != null ? com_acelerazg_users_User__CPF_CNPJ.hashCode() : 0)
+        return result
+    }
+
     @Override
     String toString() {
         "Candidate name: ${this.name}\n" +
--- src/main/groovy/com/acelerazg/users/Company.groovy
+++ src/main/groovy/com/acelerazg/users/Company.groovy
@@ -22,6 +22,33 @@ class Company implements User{
         this.likes = map['likes']
     }
 
+    boolean equals(o) {
+        if (this.is(o)) return true
+        if (!(o instanceof Company)) return false
+
+        Company company = (Company) o
+
+        if (com_acelerazg_users_User__CEP != company.com_acelerazg_users_User__CEP) return false
+        if (com_acelerazg_users_User__CPF_CNPJ != company.com_acelerazg_users_User__CPF_CNPJ) return false
+        if (com_acelerazg_users_User__country != company.com_acelerazg_users_User__country) return false
+        if (com_acelerazg_users_User__desc != company.com_acelerazg_users_User__desc) return false
+        if (com_acelerazg_users_User__email != company.com_acelerazg_users_User__email) return false
+        if (com_acelerazg_users_User__name != company.com_acelerazg_users_User__name) return false
+
+        return true
+    }
+
+    int hashCode() {
+        int result
+        result = (com_acelerazg_users_User__name != null ? com_acelerazg_users_User__name.hashCode() : 0)
+        result = 31 * result + (com_acelerazg_users_User__desc != null ? com_acelerazg_users_User__desc.hashCode() : 0)
+        result = 31 * result + (com_acelerazg_users_User__email != null ? com_acelerazg_users_User__email.hashCode() : 0)
+        result = 31 * result + (com_acelerazg_users_User__country != null ? com_acelerazg_users_User__country.hashCode() : 0)
+        result = 31 * result + (com_acelerazg_users_User__CEP != null ? com_acelerazg_users_User__CEP.hashCode() : 0)
+        result = 31 * result + (com_acelerazg_users_User__CPF_CNPJ != null ? com_acelerazg_users_User__CPF_CNPJ.hashCode() : 0)
+        return result
+    }
+
     @Override
     String toString() {
         "Company name: ${this.name}\n" +
